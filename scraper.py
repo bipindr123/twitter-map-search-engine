6fb9a605edb7acf09b744078457d2db9fbcd905b
Bipin D R <bipindr123@gmail.com>
2022-03-05 01:12:51 -0800

    scaper fixed

--- scraper.py
+++ scraper.py
@@ -1,36 +1,51 @@
 from time import sleep
+from typing import final
 import twint
 import json
 import aiohttp
+import pandas
 
 c = twint.Config()
 with open('list_cities.json',"r") as f:
     i = json.load(f)
-
+final_df = pandas.DataFrame()
 def search_tweets(c):
+    global final_df
     try:
         twint.run.Search(c)
+        # df = twint.storage.panda.Tweets_df
+        # final_df = final_df.append(df)
+        
+
     except aiohttp.client_exceptions.ClientOSError:
         sleep(60)
         print("client os exception on "+c.geo)
         search_tweets(c)
 
+    except Exception as e:
+        print(e)
+
 for idx,j in enumerate(i.values()):
-    if idx < 632:
-        continue
-    geo = str(j[0])+","+str(j[1])+", 100km"
+    tweet = {}
+    geo = str(j[0])+","+str(j[1])+", 500km"
     print(idx, geo)
+    # c.Custom["user"] = ["id", "username"]
+    c.Custom["tweet"] = ["id", "created_at", "user_id", "username", "name", "tweet" , "place", "link", "urls", "hashtags", "geo", "language"]
     c.Geo = geo
     c.Since = '2010-01-01'
-    c.Limit = 5000
+    c.Limit = 100
     c.Hide_output = True
-    c.Store_json =True
     c.Lang = 'en'
-    c.Output = "data_tweets2.json"
-    c.Custom["tweet"] = ["id", "created_at", "user_id", "username", "name", "text" , "place", "link", "urls", "hashtags", "geo"]
+    # c.Count = True
+    # c.Store_object = True
+    # c.Store_object_tweets_list = tweet
+    c.Store_json = True
+    c.Output = "data_tweets3.json"
+    # c.Pandas = True
     try:
         search_tweets(c)
     except KeyboardInterrupt:
         exit(0)
-    except:
+    except Exception as e:
+        print(e)
         continue
